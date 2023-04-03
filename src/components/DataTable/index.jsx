af1ca3cb6cb3c38f50cc9b1b0c7cb5d9ac3fbecb
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2023-04-03 11:21:20 +0200

    Ajouter le filtre et le tri à la Datatable

--- src/components/DataTable/index.jsx
+++ src/components/DataTable/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
 import { EmployeesContext } from '../../utils/context';
 import styled from 'styled-components';
 import {
@@ -8,25 +9,11 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TablePagination,
   Paper,
   TextField,
+  TableSortLabel,
+  TablePagination,
 } from '@mui/material';
-
-// Création des données pour la datatable
-const rows = [
-  {
-    firstname: 'John',
-    lastname: 'Doe',
-    startDate: '01/01/2001',
-    department: '1',
-    birthDate: '01/01/1984',
-    street: '123 Main St',
-    city: 'Anytown',
-    federal: 'AK',
-    zipcode: '99999',
-  },
-];
 
 // Style pour la zone de recherche/filtrage
 const FilterContainer = styled.div`
@@ -35,7 +22,22 @@ const FilterContainer = styled.div`
   margin-bottom: 1em;
 `;
 
-const Datatable = () => {
+/**
+ * Formate une date au format 'yyyy-mm-dd' en une date au format 'dd/mm/yyyy' en français.
+ * @param {string} dateString - La chaîne de caractères contenant la date au format 'yyyy-mm-dd'
+ *@returns {string} - La date formatée au format 'dd/mm/yyyy'
+ */
+function formatDate(dateString) {
+  const dateArray = dateString.split('-'); // Split la chaîne de caractères en un tableau
+  const year = dateArray[0];
+  const month = dateArray[1] - 1; // Les mois en JavaScript commencent à 0
+  const day = dateArray[2];
+  const dateObject = new Date(year, month, day); // Crée un objet Date
+  const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+  return dateObject.toLocaleDateString('fr-FR', options); // Renvoie une chaîne de caractères formatée en français
+}
+
+const Datatable = ({ columns }) => {
   /**
    * Déclare une variable d'état 'users' pour la liste des utilisateurs et une fonction de mise à jour 'setUsers'
    * @typedef {Array.<Object>} users - Cette variable de State contient la liste des utilisateurs
@@ -51,41 +53,109 @@ const Datatable = () => {
    */
   const { department, setDepartment } = useContext(EmployeesContext);
 
-  const [filter, setFilter] = useState('');
+  /**
+   * Utilisé pour stocker et mettre à jour le texte de filtre entré par l'utilisateur.
+   * @typedef {string} filterText - Cette variable de State contient la filtre saisi par l'utilisateur
+   * @typedef {Function} setFilterText - Cette fonction met à jour le State local
+   */
+  const [filterText, setFilterText] = useState('');
+
+  /**
+   * Définit l'état de tri des données dans un tableau et permet de le mettre à jour.
+   * @typedef {string} sortConfig - Le nom de la colonne selon laquelle trier les données et la direction de tri ('asc' pour croissant ou 'desc' pour décroissant)..
+   * @typedef {Function} setSortConfig - Cette fonction met à jour le State local
+   */
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+
+  /**
+   * Déclare un état local "page" pour le numéro de la page courante.
+   * @typedef {number} page - La page par défaut est la zéro
+   * @typedef {Function} setPage - Cette fonction met à jour le State local
+   */
   const [page, setPage] = useState(0);
+
+  /**
+   * État de la quantité de lignes à afficher par page dans une grille de données.
+   * @typedef {number} rowsPerPage - Quantité de lignes
+   * @typedef {Function} setRowsPerPage - Cette fonction met à jour le State local
+   */
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
+    setFilterText(event.target.value);
+    setPage(0);
   };
 
+  /**
+   * Met à jour le texte de filtre et réinitialise la page courante à 0.
+   * @param {Object} event - L'événement de changement de la valeur de l'élément de filtre.
+   * @returns {void}
+   */
+  const handleSort = (key) => {
+    let direction = 'asc';
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'asc'
+    ) {
+      direction = 'desc';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  /**
+   * Trie les données du tableau des utilisateurs en fonction de la configuration de tri actuelle et renvoie un nouveau tableau trié avec la configuration de tri actuelle qui contient une clé de tri et une direction de tri ('asc' ou 'desc').
+   * @returns {Array} Un nouveau tableau trié en fonction de la configuration de tri actuelle.
+   */
+  const sortedData = users.sort((a, b) => {
+    if (sortConfig !== null) {
+      const key = sortConfig.key;
+      if (a[key] < b[key]) {
+        return sortConfig.direction === 'asc' ? -1 : 1;
+      }
+      if (a[key] > b[key]) {
+        return sortConfig.direction === 'asc' ? 1 : -1;
+      }
+    }
+    return 0;
+  });
+
+  /**
+   * Filtre les données triées en fonction du texte de filtre entré par l'utilisateur
+   * @returns {Array<Object>} Les données triées filtrées
+   */
+  const filteredData = sortedData.filter((row) =>
+    Object.keys(row).some((key) =>
+      row[key].toString().toLowerCase().includes(filterText.toLowerCase())
+    )
+  );
+
+  /**
+   * Gère le changement de page de la pagination.
+   * @param {object} event - L'événement déclencheur.
+   * @param {number} newPage - Le numéro de la nouvelle page.
+   * @returns {void}
+   */
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
+  /**
+   * Gère le changement du nombre de lignes affichées par page dans le tableau.
+   * @param {object} event - L'événement de changement de la valeur de sélection.
+   */
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
-  // Filtrage des données
-  const filteredRows = users.filter((user) => {
-    return (
-      user.firstname.toLowerCase().includes(filter.toLowerCase()) ||
-      user.lastname.toLowerCase().includes(filter.toLowerCase())
-    );
-  });
-
-  const startIndex = page * rowsPerPage;
-  const endIndex = (page + 1) * rowsPerPage;
-
   return (
     <div>
       <FilterContainer>
         <TextField
-          label="Filtrer"
           variant="outlined"
-          value={filter}
+          label="Filter"
+          value={filterText}
           onChange={handleFilterChange}
         />
       </FilterContainer>
@@ -93,44 +163,57 @@ const Datatable = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Start Date</TableCell>
-              <TableCell>Department</TableCell>
-              <TableCell>Date of Birth</TableCell>
-              <TableCell>Street</TableCell>
-              <TableCell>City</TableCell>
-              <TableCell>State</TableCell>
-              <TableCell>Zip Code</TableCell>
+              {columns.map((column) => (
+                <TableCell key={column.field}>
+                  <TableSortLabel
+                    active={sortConfig && sortConfig.key === column.field}
+                    direction={
+                      sortConfig && sortConfig.key === column.field
+                        ? sortConfig.direction
+                        : 'asc'
+                    }
+                    onClick={() => handleSort(column.field)}
+                  >
+                    {column.headerName}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {filteredRows.slice(startIndex, endIndex).map((row, index) => (
-              <TableRow key={1000 + index}>
-                <TableCell>{row.firstname}</TableCell>
-                <TableCell>{row.lastname}</TableCell>
-                <TableCell>{row.startDate}</TableCell>
-                <TableCell>
-                  {
-                    /** Jointure libellé */
-                    department.find((dep) => dep.id === row.department)?.name ||
-                      ''
-                  }
-                </TableCell>
-                <TableCell>{row.birthDate}</TableCell>
-                <TableCell>{row.street}</TableCell>
-                <TableCell>{row.city}</TableCell>
-                <TableCell>{row.federal}</TableCell>
-                <TableCell>{row.zipcode}</TableCell>
-              </TableRow>
-            ))}
+            {filteredData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => (
+                <TableRow key={1000 + index}>
+                  {columns.map((column) =>
+                    column.field === 'department' ? (
+                      <TableCell key={column.field}>
+                        {
+                          /** Jointure libellé */ department.find(
+                            (dep) => dep.id === row.department
+                          )?.name || ''
+                        }
+                      </TableCell>
+                    ) : column.field === 'startDate' ||
+                      column.field === 'birthDate' ? (
+                      <TableCell key={column.field}>
+                        {formatDate(row[column.field])}
+                      </TableCell>
+                    ) : (
+                      <TableCell key={column.field}>
+                        {row[column.field]}
+                      </TableCell>
+                    )
+                  )}
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={filteredRows.length}
+        count={filteredData.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
@@ -138,6 +221,15 @@ const Datatable = () => {
       />
     </div>
   );
+};
+
+Datatable.propTypes = {
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      field: PropTypes.string.isRequired,
+      headerName: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Datatable;
--- src/components/Tab2Content/index.jsx
+++ src/components/Tab2Content/index.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import Table from '../DataTable';
+import DataTable from '../DataTable';
 
 function Tab2Content() {
+  /**
+   * Liste des colonnes pour la DataTable
+   * @typedef {Object} Column
+   * @property {string} field - Nom du champ associé à la colonne
+   * @property {string} headerName - Titre de la colonne
+   */
+  /**
+   *Les colonnes à afficher dans la table
+   * @type {Column[]}
+   */
+  const columns = [
+    { field: 'firstname', headerName: 'First Name' },
+    { field: 'lastname', headerName: 'Last Name' },
+    { field: 'startDate', headerName: 'Start Date' },
+    { field: 'department', headerName: 'Department' },
+    { field: 'birthDate', headerName: 'Date of Birth' },
+    { field: 'street', headerName: 'Street' },
+    { field: 'city', headerName: 'City' },
+    { field: 'federal', headerName: 'State' },
+    { field: 'zipcode', headerName: 'Zip Code' },
+  ];
+
   return (
     <React.Fragment>
       <h2>Current Employees</h2>
-      <Table />
+      <DataTable columns={columns} />
     </React.Fragment>
   );
 }
